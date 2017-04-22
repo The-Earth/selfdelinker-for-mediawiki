c9867335965020c2cd0761c8a8e498e17a99f797
lziad <daizl@pku.edu.cn>
2017-04-22 21:55:18 +0800

    Updating regex

--- selflinktagger.py
+++ selflinktagger.py
@@ -3,8 +3,7 @@ import re
 import time
 
 site = mwclient.Site("zh.wikipedia.org")
-reg = r"(\[\[\w+]]|\[\[\w+\|\w+]]|\[\[\w+\s\(\w+\)]]|\[\[\w+\s\(\w+\)\|\w+]])"
-tagNeed,num1,num2 = False,0,0
+reg = re.compile(r'\[\[\s*:?(.*?)(\|[\s\S]*?)?]]')
 
 while True:
     try:
@@ -18,25 +17,20 @@ for pagegen in site.random(0, limit=20):
     pageTitle = pagegen["title"]
     page = site.pages[pageTitle]
     text = page.text()
-    match = re.findall(reg, text)
+    tagNeed,num1,num2 = False,0,0
 
-    for link in match:
-        if link == "[[" + pageTitle + "]]":
-            num1 = text.count(link)
+    for link in reg.finditer(text):
+        if link.group(1).strip() == pageTitle:
+            if link.group(2) and link.group(2) != '|':
+                num2 += 1
+            else:
+                num1 += 1
             tagNeed = True
-            continue
-        elif "|" in link:
-            par = link.split("|")
-            if par[0][2:] == pageTitle:
-                num2 = text.count("[["+pageTitle+"|")
-                tagNeed = True
-                continue
 
     if tagNeed:
         with open("selflinklist.txt","a") as log:
-            log.write("#[["+pageTitle+"]]"+str(num1)+"个<nowiki>[[title]]<nowiki>，"+str(num2)+"个<nowiki>[[title|text]]</nowiki>\n")
+            log.write("#[[%s]]，%d个<nowiki>[[title]]</nowiki>，%d个<nowiki>[[title|text]]</nowiki>\n" % (pageTitle， num1, num2))
         print(pageTitle + " tagged.")
-        tagNeed = False
     else:
         print("No selflinks in " + pageTitle)
     time.sleep(1)
