31acb7b58e20565ef0a0b38580de1160b15a5e46
The-Earth <tigerzeng97@gmail.com>
2017-04-23 16:43:20 +0800

    Merge from uploader
    将uploader的功能合并进来。本地文件中出现10条记录则上传。

--- selflinktagger.py
+++ selflinktagger.py
@@ -1,9 +1,11 @@
 import mwclient
 import re
 import time
+import os
 
 site = mwclient.Site("zh.wikipedia.org")
 reg = re.compile(r'\[\[\s*:?(.*?)(\|[\s\S]*?)?]]')
+logPage = site.Pages["User:Tiger-bot/SelfLinks"]
 
 while True:
     try:
@@ -29,8 +31,25 @@ for pagegen in site.random(0, limit=20):
 
     if tagNeed:
         with open("selflinklist.txt","a") as log:
-            log.write("#[[%s]]，%d个<nowiki>[[title]]</nowiki>，%d个<nowiki>[[title|text]]</nowiki>\n" % (pageTitle， num1, num2))
+            log.write("#[[%s]]，%d个<nowiki>[[title]]</nowiki>，%d个<nowiki>[[title|text]]</nowiki>\n" % (pageTitle, num1, num2))
         print(pageTitle + " tagged.")
     else:
         print("No selflinks in " + pageTitle)
+
+    with open("selflinklist.txt", "r") as file:
+        upload = False
+        li = file.readlines()
+        if len(li) >= 10:
+            upload = True
+
+    if upload:
+        with open("selflinklist.txt", "r") as file:
+            txt = file.read()
+        oldtext = logPage.text()
+        newtext = oldtext + "\n----\n" + txt
+        logPage.save(newtext, "机器人：上传自链接检测记录。", minor=True)
+        print("Log saved in https://zh.wikipedia.org/wiki/User:Tiger-bot/SelfLinks")
+
+        with open("selflinklist.txt", "w") as file:
+            file.write("")
     time.sleep(1)
